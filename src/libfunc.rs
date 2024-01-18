59f59054d55421a5f51da5bb1b793f9fab1dfae2
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-18 09:03:12 -0500

    use rust, actually

---
+++ src/libfunc.rs
@@ -0,0 +1,1 @@
+// TODO: add functions to select inputs
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod liblists;
+mod libfunc;
+use liblists::*, libfunc::*;
+
+fn main() {
+    println!("Hello, world!")
+
+    // TODO: use functions from libfunc in a while loop
+}
