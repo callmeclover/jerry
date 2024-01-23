eb6638e7c140697296eab9c31ebca46b0fbf1b77
Clover Johnson <95094165+callmeclover@users.noreply.github.com>
2024-01-23 13:11:46 +0000

    dev 8

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod libfunc;
 mod libconf;
+//mod liblists;
 use libconf::*;
 use libfunc::main_logic;
 
-use tts::*;
+//use tts::*;
 use enigo::*;
 
 #[tokio::main]
@@ -11,10 +12,10 @@ async fn main() {
     let config = get_config().await;
     let options = get_options(config).await;
 
-    let mut tts = Tts::default();
+//    let mut tts = Tts::default();
     let mut enigo = Enigo::new();
 
     loop {
-        main_logic(options, tts, enigo).await;
+        main_logic(&options, /*tts, */&mut enigo).await;
     }
 }
